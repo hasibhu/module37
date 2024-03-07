1c8261fcc21ee7e231b9424728cc684e6f6514ee
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-03-07 11:30:42 +0100

    first commit

---
+++ fundamentals.js
@@ -0,0 +1,30 @@
+
+
+// how to declare a variable using let and const
+
+const fatherName = "Saken";
+
+let season = "rainy"
+season = "winter"
+
+const multiply = (a, b) => a * b;
+
+
+// console.log(multiply(4, 6));
+
+// accessing the property of an object -3 ways
+
+const student = {
+    name: 'hasib',
+    age : 34,
+    course : ['biology', 'neuroscience', 'psychology'] 
+}
+// 1
+const myVariable = 'age'
+console.log(student.age);
+
+console.log(student['age']);
+console.log(student[myVariable]);
+
+
+
